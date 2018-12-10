ff4fce67524dc4a2c3c9a0903a01c09cd4d0a133
duckhee <fain9301@yahoo.com>
2018-12-10 11:37:22 +0900

    testing

--- Src/main.c
+++ Src/main.c
@@ -1,4 +1,71 @@
-int main()
+/*
+ * (C) COPYRIGHT 2009 CRZ
+ *
+ * File Name : main.c
+ * Author    : POOH
+ * Version   : V1.0
+ * Date      : 08/12/2009
+ */
+
+/* includes */
+
+//#include "hw_config.h"
+#include "main_menu.h"
+
+
+/* global variables */
+
+RCC_ClocksTypeDef  rcc_clocks;
+
+bool g_TestProcessState = FALSE;
+
+/* functions */
+
+
+
+/*
+ * Name   : main
+ * Input  : None
+ * Output : None
+ * Return : None
+ */
+int main(void)
 {
-	
+
+
+    /* System Clocks Configuration */
+    RCC_Configuration();
+
+    RCC_GetClocksFreq(&rcc_clocks);
+
+    /* NVIC configuration */
+    NVIC_Configuration();
+
+    /* Configure the GPIO ports */
+    GPIO_Configuration();
+
+    /* EXTI configuration */
+    EXTI_Configuration();
+
+    /* UART initialization */
+    USART1_Init();
+
+    /* Setup SysTick Timer for 1 msec interrupts  */
+    if (SysTick_Config(rcc_clocks.SYSCLK_Frequency / 1000))
+    { 
+        /* Capture error */ 
+        while (1);
+    }
+
+    USB_Cable_Config(DISABLE);
+
+    Delay(500);
+    LED_Off_All();
+    while(1)
+    {
+        default_menu();
+    }
+    Delay(1000);
+    
+    return 0;
 }
